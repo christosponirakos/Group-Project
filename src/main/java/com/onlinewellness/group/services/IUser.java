14adea6e6bdeb4bd3f8b29aa83db8f75d5e3c6ec
Christos Ponirakos <cponirakos@gmail.com>
2020-05-15 10:18:35 +0300

    uploading AppConfig, AppInitializer, HibernateConfiguration, UserController, AbstractDao, User, IUser

--- src/main/java/com/onlinewellness/group/services/IUser.java
+++ src/main/java/com/onlinewellness/group/services/IUser.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.onlinewellness.group.dao;
+package com.onlinewellness.group.services;
 
 /**
  *
  * @author Chris
  */
-public class NewClasstest {
+public interface IUser {
     
 }
