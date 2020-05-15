f0869ab04460718f6fabfa96f16c2ac92422fea6
Christos Ponirakos <cponirakos@gmail.com>
2020-05-15 08:59:16 +0300

    initial commit
    project structure from spring initializer. Spring boot. 
    WEB-INF folder was added

---
+++ src/test/java/com/onlinewellness/group/GroupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinewellness.group;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
