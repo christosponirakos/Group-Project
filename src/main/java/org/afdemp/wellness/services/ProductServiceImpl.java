a65aabfad29b2b0033e722e3e239174e124506ef
Christos Ponirakos <cponirakos@gmail.com>
2020-05-23 22:24:17 +0300

    adding ProductServiceImpl

---
+++ src/main/java/org/afdemp/wellness/services/ProductServiceImpl.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.afdemp.wellness.services;
+
+import java.util.List;
+import org.afdemp.wellness.entities.Product;
+
+/**
+ *
+ * @author Chris
+ */
+
+public class ProductServiceImpl implements IProductService{
+
+    @Override
+    public List<Product> findAllProducts() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Product findById(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean save(Product product) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(Product product) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
